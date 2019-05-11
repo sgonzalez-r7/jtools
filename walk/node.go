17487baa6073c979050be58a72c0f2ebee767f41
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2019-05-11 12:38:04 -0500

    Add Node Squash method

--- walk/node.go
+++ walk/node.go
@@ -15,21 +15,28 @@ type Node struct {
 
 type Base []interface{}
 
-func (b Base) Path(keep bool) string {
-	var k string
+func (b Base) Path(sep string) string {
 	buf := make([]string, 0, len(b))
 	for _, v := range b {
 		switch val := v.(type) {
 		case string:
-			k = val
+			buf = append(buf, val)
 		case int:
-			i := `i`
-			if keep {
-				i = strconv.Itoa(val)
-			}
-			k = `[`+ i +`]`
+			buf = append(buf, `[`+ strconv.Itoa(val) + `]`)
 		}
-		buf = append(buf, k)
 	}
-	return `/`+ strings.Join(buf, `/`)
+	return strings.Join(buf, sep)
+}
+
+func (b Base) Squash(sep string) string {
+	buf := make([]string, 0, len(b))
+	for _, v := range b {
+		switch val := v.(type) {
+		case string:
+			buf = append(buf, val)
+		case int:
+			buf = append(buf, `[i]`)
+		}
+	}
+	return strings.Join(buf, sep)
 }
